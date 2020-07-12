76bc5d11dbfea51166b48b36815f2976eb71faeb
Marylange <marylangess@gmail.com>
2020-07-12 14:57:28 -0300

    estilo pokedex

--- bloco11/11.2/mocks_2.js
+++
@@ -1,8 +0,0 @@
-// Com a mesma função do exercício anterior, utilizando o 
-// mock, crie uma nova implementação que deve receber dois parâmetros e 
-// retornar a divisão do primeiro pelo segundo. Essa implementação deve 
-// ocorrer uma única vez. Faça os testes necessários.
-const divisaoNumero = (num1, num2) => num1 / num2;
-const numeroAleatorio = () => Math.round(Math.random()* 100);
-
-module.exports = { numeroAleatorio, divisaoNumero };
